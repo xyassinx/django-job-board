d7aaf84f7cbe2bbde14a8cde7fb91485092b4ba1
yassin <yassino.khaled141@gmail.com>
2024-12-06 07:45:11 +0200

    wo

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # myapps
+    'job',
+    'blog',
+    'accounts',
+    'contact',
+    'home',
 ]
 
 MIDDLEWARE = [
